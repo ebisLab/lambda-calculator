3c2b378400ceeb3203d11c145b18a6a3cebbd540
ebisLab <ebaiden@techie.com>
2019-07-17 10:37:27 -0400

    i passed the props operator and nothing broke

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -8,11 +8,14 @@ import {operators} from '../../../data'
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+
+  const [operatorData, setOperatorData] = useState(operators)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorData.map(operator => <OperatorButton operator={operator} />)}
     </div>
   );
 };
